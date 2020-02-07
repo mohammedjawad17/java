98ee132a21f3c8e77d3643206eba1676b474f69d
mohammedjawad17 <60223832+mohammedjawad17@users.noreply.github.com>
2020-02-07 15:48:27 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,11 +1,11 @@
 class helloworld
 {
-//Your program begins with a call to main().
-//Prints"Hello,World" to the terminal window.
-public static void main(Sting args[])
-{
-System.out.println("Hello world and welcome to java");
-System.out.println("we are checking whether java is installed/not");
-System.out.println("Developer will check their source code in this way");
-}
+  //Your program begins with a call to main().
+  //Prints "Hello,World" to the terminal window.
+  public static void main(String args[])
+  {
+    System.out.println("Hello world and welcome to java");
+    System.out.println("we are checking whether java is installed/not");
+    System.out.println("Developer will check their source code in this way");
+   }
 }
